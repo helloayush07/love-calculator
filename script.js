3263929d6312d850881cc80717382f2879a86ded
Ayush <74857388+helloayush07@users.noreply.github.com>
2024-01-14 22:35:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// var firebaseConfig = {
+// // nah, I'm a nice guy :)
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+// // Reference messages collection
+// var friendsRef = firebase.database().ref("friends");
+
+// Listen for submit
+document.getElementById("dataForm").addEventListener("submit", e => {
+  e.preventDefault();
+
+  // get values
+  var personName = getInputVal("personName");
+  var personGender = getInputVal("personGender");
+  var crushName = getInputVal("crushName");
+  var crushGender = getInputVal("crushGender");
+  var ageDiff = getInputVal("ageDiff");
+  var yearDiff = getInputVal("yearDiff");
+
+  // saveData(personName, personGender, crushName, crushGender, ageDiff, yearDiff);
+  document.querySelector(".main").style.display = 'none'
+
+  document.querySelector('.calculating').style.display = 'flex'
+  setTimeout(()=>{
+    document.querySelector('.calculating').style.display = 'none'
+    document.querySelector('.final').style.display = 'block'
+    console.log("Well don't worry mister, The data's not saved. :) No post request have been made. Chill!")
+  })
+  openVideoPopup()
+
+  
+});
+
+// Function to get form values:
+const getInputVal = id => {
+  return document.getElementById(id).value;
+};
+
+
+
+// Function to save data to firebase
+// const saveData = (
+//   personName,
+//   personGender,
+//   crushName,
+//   crushGender,
+//   ageDiff,
+//   yearDiff
+// ) => {
+//   let newFriendsRef = friendsRef.push();
+
+//   newFriendsRef.set({
+//     personName,
+//     personGender,
+//     crushName,
+//     crushGender,
+//     ageDiff,
+//     yearDiff
+//   });
+// };
